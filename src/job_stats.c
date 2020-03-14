0eabe219aa6baa5aacf19fc10b219c04280ec0d5
Zewei Chen <czwking1991@gmail.com>
2020-03-14 16:12:17 -0400

    --

--- src/job_stats.c
+++ src/job_stats.c
@@ -20,6 +20,7 @@ int log_preemptions[MAX_LOG];
 int log_migrations[MAX_LOG];
 int max_preemption = 0;
 int max_migration = 0;
+int statistic = 0;
 
 static double nano_to_ms(int64_t ns)
 {
@@ -85,52 +86,54 @@ static void print_stats(
 		max_migration = migrations;
 	log_migrations[migrations]++;
 
-	if (want_ms)
-		printf(" %5u, %5u, %10.2f, %10.2f, %8d, %10.2f, %10.2f, %7d"
-			", %10.2f, %12u, %12u\n",
-		       release->rec->hdr.pid,
-		       release->rec->hdr.job,
-		       nano_to_ms(per(t)),
-		       nano_to_ms(response),
-		       lateness > 0,
-		       nano_to_ms(lateness),
-		       lateness > 0 ? nano_to_ms(lateness) : 0,
-		       completion->rec->data.completion.forced,
-		       nano_to_ms(completion->rec->data.completion.exec_time),
-		       preemptions,
-		       migrations);
-	else
-		printf(" %5u, %5u, %10llu, %10llu, %8d, %10lld, %10lld, %7d"
-			", %10llu, %12u, %12u\n",
-		       release->rec->hdr.pid,
-		       release->rec->hdr.job,
-		       (unsigned long long) per(t),
-		       (unsigned long long) response,
-		       lateness > 0,
-		       (long long) lateness,
-		       lateness > 0 ? (long long) lateness : 0,
-		       completion->rec->data.completion.forced,
-		       (unsigned long long) completion->rec->data.completion.exec_time,
-		       preemptions,
-		       migrations);
+	if (!statistic)
+		if (want_ms)
+			printf(" %5u, %5u, %10.2f, %10.2f, %8d, %10.2f, %10.2f, %7d"
+				", %10.2f, %12u, %12u\n",
+						release->rec->hdr.pid,
+						release->rec->hdr.job,
+						nano_to_ms(per(t)),
+						nano_to_ms(response),
+						lateness > 0,
+						nano_to_ms(lateness),
+						lateness > 0 ? nano_to_ms(lateness) : 0,
+						completion->rec->data.completion.forced,
+						nano_to_ms(completion->rec->data.completion.exec_time),
+						preemptions,
+						migrations);
+		else
+			printf(" %5u, %5u, %10llu, %10llu, %8d, %10lld, %10lld, %7d"
+				", %10llu, %12u, %12u\n",
+						release->rec->hdr.pid,
+						release->rec->hdr.job,
+						(unsigned long long) per(t),
+						(unsigned long long) response,
+						lateness > 0,
+						(long long) lateness,
+						lateness > 0 ? (long long) lateness : 0,
+						completion->rec->data.completion.forced,
+						(unsigned long long) completion->rec->data.completion.exec_time,
+						preemptions,
+						migrations);
 }
 
 static void print_task_info(struct task *t)
 {
-	if (want_ms)
-		printf("# task NAME=%s PID=%d COST=%.2f PERIOD=%.2f CPU=%d\n",
-		       tsk_name(t),
-		       t->pid,
-		       nano_to_ms(exe(t)),
-		       nano_to_ms(per(t)),
-		       tsk_cpu(t));
-	else
-		printf("# task NAME=%s PID=%d COST=%lu PERIOD=%lu CPU=%d\n",
-		       tsk_name(t),
-		       t->pid,
-		       (unsigned long) exe(t),
-		       (unsigned long) per(t),
-		       tsk_cpu(t));
+	if (!statistic)
+		if (want_ms)
+			printf("# task NAME=%s PID=%d COST=%.2f PERIOD=%.2f CPU=%d\n",
+						tsk_name(t),
+						t->pid,
+						nano_to_ms(exe(t)),
+						nano_to_ms(per(t)),
+						tsk_cpu(t));
+		else
+			printf("# task NAME=%s PID=%d COST=%lu PERIOD=%lu CPU=%d\n",
+						tsk_name(t),
+						t->pid,
+						(unsigned long) exe(t),
+						(unsigned long) per(t),
+						tsk_cpu(t));
 }
 
 static void usage(const char *str)
@@ -141,11 +144,12 @@ static void usage(const char *str)
 		"    st_job_stats [opts] <file.st>+\n"
 		"\n"
 		"  OPTIONS\n"
-		"     -r         -- skip jobs prior to task-system release\n"
-		"     -m         -- output milliseconds (default: nanoseconds)\n"
-		"     -p PID     -- show only data for the task with the given PID\n"
-		"     -n NAME    -- show only data for the task(s) with the given NAME\n"
-		"     -t PERIOD  -- show only data for the task(s) with the given PERIOD\n"
+		"     -r        	  -- skip jobs prior to task-system release\n"
+		"     -m        	  -- output milliseconds (default: nanoseconds)\n"
+		"     -p PID        -- show only data for the task with the given PID\n"
+		"     -n NAME       -- show only data for the task(s) with the given NAME\n"
+		"     -t PERIOD     -- show only data for the task(s) with the given PERIOD\n"
+		"     -s STATISTICS -- show statistics\n"
 		"\n\n"
 		);
 	if (str) {
@@ -201,6 +205,9 @@ int main(int argc, char** argv)
 		case 'n':
 			name_filter = optarg;
 			break;
+		case 's':
+			statistic = 1;
+			break;
 		case 'h':
 			usage(NULL);
 			break;
@@ -236,6 +243,7 @@ int main(int argc, char** argv)
 	}
 
 	/* print header */
+	if (!statistic)
 	printf("#%5s, %5s, %10s, %10s, %8s, %10s, %10s, %7s, %10s, %12s, %12s\n",
 	       "Task",
 	       "Job",
@@ -281,18 +289,20 @@ int main(int argc, char** argv)
 		}
 	}
 
-	printf("Total number of jobs:%d\n", total_jobs);
-	printf("Unsched:%d\n", unsched);
-	sched_ratio = total_jobs - unsched;
-	sched_ratio /= total_jobs;
-	printf("Sched Ratio:%f\n", sched_ratio);
+	if (statistic) {
+		printf("Total number of jobs:%d\n", total_jobs);
+		printf("Unsched:%d\n", unsched);
+		sched_ratio = total_jobs - unsched;
+		sched_ratio /= total_jobs;
+		printf("Sched Ratio:%f\n", sched_ratio);
 
-	for (int i = 0; i < max_preemption; i++) {
-		printf("Number of jobs that were preempted %d times: %d\n", i, log_preemptions[i]);
-	}
+		for (int i = 0; i < max_preemption; i++) {
+			printf("Number of jobs that were preempted %d times: %d\n", i, log_preemptions[i]);
+		}
 
-	for (int i = 0; i < max_migration; i++) {
-		printf("Number of jobs that migrated %d times: %d\n", i, log_migrations[i]);
+		for (int i = 0; i < max_migration; i++) {
+			printf("Number of jobs that migrated %d times: %d\n", i, log_migrations[i]);
+		}
 	}
 
 	return 0;
